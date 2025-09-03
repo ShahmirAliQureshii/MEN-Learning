744a39e6b91917734040126e6ef19635937c3747
ShahmirAliQureshii <shahmirali385@gmail.com>
2025-09-03 20:02:52 +0500

    MEN Learning

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
-app.set("view engine", 'ejs')
+app.set("view engine", "ejs");
 
+app.use(morgan('dev'));
 
 app.use((req, res, next)=>{
     console.log("This is middleware");
-    const a = 2;
-    const b = 3;
+    const a = 2
+    const b = 4;
     console.log(a+b);
-    return next()
+    return next();
+
 })
 
-app.get("/", (req, res)=>{
-    res.render('index')
-});
-app.get('/about', (req, res)=>{
-    res.send("Its me, Shahmir Ali Qureshi!");
+app.get('/',(req,res)=>{
+    res.send("Bas bhai thak gaya")
 })
-app.get('/profile', (req, res)=>{
-    res.send("This is the Profile Of Shahmir Ali Qureshi!")
+app.get("/persons",(req, res)=>{
+    res.send(["Shahmir", "Shayan", "Mateen", "Ayaan"]);
+})
+app.get('/res',(req,res)=>{
+    res.render("index");
 })
 app.listen(3000)
